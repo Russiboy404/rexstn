563d3fbff65a43aace14df1af3419310d48d6ea4
Russi <98789401+Russiboy404@users.noreply.github.com>
2022-09-29 19:33:59 +0300

    Add files via upload

---
+++ rexstn.py
@@ -0,0 +1,156 @@
+import os
+import time
+import sys
+import requests
+def chk():
+	
+    uuid = str(os.geteuid()) + str(os.getlogin())
+    id = '-'.join(uuid)
+    print '\x1b[37;1mIDY XOT : ' + id
+    
+    try:
+        httpCaht = requests.get('https://github.com/Russiboy404/dr/blob/main/Id.txt').text
+        if id in httpCaht:
+            print '\x1b[92mDLY MN IDIY ACTIVA.........'
+            msg = str(os.geteuid())
+            time.sleep(2)
+        else:
+            print '\x1b[91mBarezm Id kat active nya Tkaya bo Active krdn nama bnera bo telegram @sell_pubg9.......'
+            time.sleep(2)
+            sys.exit()
+    except:
+        sys.exit()
+        if name == '__main__':
+            print logo
+            chk()
+            
+chk()
+os.system("clear")
+
+print('''\033[91m
+   _   _   _   _  
+  / \ / \ / \ / \ 
+ ( D | A | R | K )
+  \_/ \_/ \_/ \_/ 
+                                                                                                                                                                                                                              
+CREATED BY DARK
+''')
+def slowprint(s):
+    for c in s + '\n' :
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(5. / 100)
+
+print (''' \033[95m
++--------------------------------------+
+| HAMW TOOLAKNI TERMX |
++--------------------------------------+
+| Coded By DARK 1.2  |
++--------------------------------------+''')
+
+
+os.system ("apt update")
+os.system ("apt upgrade -y")
+os.system ("apt install python -y")
+os.system ("apt install python2 -y")
+os.system ("apt install php -y")
+os.system ("apt install python-dev -y")
+os.system ("apt install python3 -y")
+os.system ("apt install java -y")
+os.system ("apt install git -y")
+os.system ("apt install perl -y")
+os.system ("apt install bash")
+os.system ("apt install file")
+os.system ("pip2 insatll file")
+os.system ("pip install file")
+os.system ("pkg install file")
+os.system ("apt install nano -y")
+os.system ("apt install curl -y")
+os.system ("apt install openssl -y")
+os.system ("apt install openssh -y")
+os.system ("apt install wget -y")
+os.system ("apt install clang -y")
+os.system ("apt install nmap -y")
+os.system ("apt install w3m -y")
+os.system ("apt install hydra -y")
+os.system ("apt install ruby -y")
+os.system ("apt install macchanger -y")
+os.system ("apt install host -y")
+os.system ("apt install dnsutils -y")
+os.system ("apt install coreutils -y")
+os.system ("apt install fish -y")
+os.system ("apt install zip -y")
+os.system ("apt install hydra -y")
+os.system ("apt install figlet -y")
+os.system ("apt install cowsay -y")
+os.system ("apt install unzip -y")
+os.system ("apt install vim -y")
+os.system ("apt install ruby -y")
+os.system ("apt install wcalc -y")
+os.system ("apt install bmon -y")
+os.system ("apt install unrar -y")
+os.system ("apt install proot -y")
+os.system ("apt install golang -y")
+os.system ("apt update")
+os.system ("apt upgrade")
+os.system ("pkg install update")
+os.system ("pkg install upgrade")
+os.system ("apt-get update")
+os.system ("apt-get upgrade")
+os.system ("apt install php")
+os.system ("apt install python")
+os.system ("apt install python2")
+os.system ("apt install git")
+os.system ("apt install golang")
+os.system ("apt install host ")
+os.system ("apt install nano")
+os.system ("apt install havij")
+os.system ("apt install hydra")
+os.system ("apt install wireshark")
+os.system ("apt install cmatrix")
+os.system ("pkg install wget")
+os.system ("pkg install python2-dev -y")
+os.system ("apt install wireshark ")
+os.system ("pkg install cowsay")
+os.system ("pkg install toilet")
+os.system ("pkg install figlet")
+os.system ("pkg install wget")
+os.system ("pkg install wget -y")
+os.system ("pkg install python2 -y")
+os.system ("pkg install toilet")
+os.system ("pkg install ruby")
+os.system ("pkg install help ")
+os.system ("gem install lolcat")
+os.system ("pkg install curl")
+os.system ("pkg install wgetrc")
+os.system ("pkg install unzip")
+os.system ("pkg install openssh")
+os.system ("pkg install tor")
+os.system ("pkg install uzip")
+os.system ("pkg install net-tools")
+os.system ("pkg install unrar")
+os.system ("pkg install clang")
+os.system ("pkg install w3m")
+os.system ("pkg install proot")
+os.system ("pip2 install requests")
+os.system ("pip install requests")
+os.system ("apt update && apt upgrade")
+os.system ("pkg install python")
+os.system ("pkg install python2")
+os.system ("pkg install python3")
+os.system ("pkg install python -y")
+os.system ("pkg install toilet -y")
+os.system ("pkg install bash -y")
+os.system ("pip2 install bs4")
+os.system ("pip install bs4")
+os.system ("apt install mechanize")
+os.system ("pip2 install mechanize")
+os.system ("pip install mechanize")
+os.system ("pkg install ls -y")
+os.system ("pip install bash -y")
+os.system ("pip2 install bash -y")
+os.system ("termux-setup-storage")
+
+
+
+input("\n\nEnter bka bo chwna darawaw : ")
