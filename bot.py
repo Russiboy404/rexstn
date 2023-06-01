0602ca4aa6399ba20cb0d49a720fa1c32ddcdf57
Russi <98789401+Russiboy404@users.noreply.github.com>
2023-06-02 01:45:39 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,14 @@
+import os, sys
+try:
+	import requests,os,names,json,colorama,secrets,random,pyfiglet,OneClick
+except:
+	os.system("pip install requests")
+	os.system("pip install names")
+	os.system("pip install json")
+	os.system("pip install colorama")
+	os.system("pip install secrets")
+	os.system("pip install random")
+	os.system("pip install pyfiglet")
+	os.system("pip install OneClick")
+	os.system("pip install stdiomask")
+	print("-- Done --")
